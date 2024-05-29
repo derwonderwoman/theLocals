e6e6f2dd688437ee77bd71521f153d808bfb6d5c
derwonderwoman <tokarevakseniia@gmail.com>
2024-05-29 15:27:38 +0300

    first commit

---
+++ Layout/Front-end Locals/src/components/CallForRegistration.tsx
@@ -0,0 +1,10 @@
+const CallForRegistration = () => {
+    return (
+        <div className="call">
+            <h1 id="reg">You are not registered yet?</h1>
+            <button>Sign in</button>
+        </div>
+    )
+}
+
+export default CallForRegistration;
