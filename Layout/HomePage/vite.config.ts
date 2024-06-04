cdd383406617f3ca9933b001b027c7be40c154e4
derwonderwoman <tokarevakseniia@gmail.com>
2024-06-04 15:15:04 +0300

    deploy???

---
+++ Layout/HomePage/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    port: 10000,
+  },
+  server: {
+    port: 10000,
+  }
+})
