44c2e3be283565271092e6fe263731429762dae2
derwonderwoman <tokarevakseniia@gmail.com>
2024-06-07 19:21:22 +0300

    specorders

--- server/src/models/specialists.ts
+++ server/src/models/specialists.ts
@@ -90,13 +90,13 @@ export const login = async (email: string): Promise<Specialist | undefined> => {
     }
 }
 
-export const newOrders = async (specialistId: number): Promise<Order[]> => {
+export const newOrders = async (id: number): Promise<Order[]> => {
     try {
         const orders: Order[] = await db('applications')
             .select('applications.date', 'applications.time', 'applications.rate_per_hour', 'applications.town', 'clients.first_name', 'clients.last_name')
-            .join('clients', 'applications.client_id', '=', 'clients.id')
+            .join('clients', 'applications.client_id','clients.id')
             .where('applications.status', '=', 'pending')
-            .andWhere('applications.specialisation', '=', db('specialists').select('specialisation').where('id', '=', specialistId).first());
+            .andWhere('applications.specialisation', db('specialists').select('specialisation').where('id', id).first());
         return orders;
     } catch (error) {
         console.error('Error fetching pending orders:', error);
