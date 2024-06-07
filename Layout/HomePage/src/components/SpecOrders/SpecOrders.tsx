d228efdfc131da62547d1b643be2b79c0cf092d2
derwonderwoman <tokarevakseniia@gmail.com>
2024-06-08 00:02:04 +0300

    changingStatus

--- Layout/HomePage/src/components/SpecOrders/SpecOrders.tsx
+++ Layout/HomePage/src/components/SpecOrders/SpecOrders.tsx
@@ -40,7 +40,7 @@ const SpecOrders = () => {
 
     const handleApply = async (orderId: number) => {
         try {
-            await axios.put(`${BASE_URL}/specialist/applications/:${orderId}`, {
+            await axios.put(`${BASE_URL}/specialist/applications/${orderId}`, {
                 status: 'waiting for approving',
                 specialist_id: loggedInUser.id
             }, {
