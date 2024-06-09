399c177dbadc2c01281b5f72f035887f9b9f3a94
derwonderwoman <tokarevakseniia@gmail.com>
2024-06-09 20:33:08 +0300

    logout

--- Layout/HomePage/src/components/HomePage/Logout.tsx
+++ Layout/HomePage/src/components/HomePage/Logout.tsx
@@ -1,31 +1,38 @@
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../App";
 
 const LogOut = () => {
     const { setLoggedInUser } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [showConfirmation, setShowConfirmation] = useState(false);
 
     const handleLogout = () => {
         setLoggedInUser({
-            id:0,
-            email:"",
-            first_name:"",
-            last_name:"",
-            phone_number:"",
-            town:"",
-            type:"client",
+            id: 0,
+            email: "",
+            first_name: "",
+            last_name: "",
+            phone_number: "",
+            town: "",
+            type: "client",
             token: ""
         });
     
         localStorage.removeItem('loggedInUser');
-        alert("You've been logged out")
         navigate("/client/login");
     }
 
     return (
         <div>
-            <button onClick={handleLogout} id="loginspec">Log Out</button>
+            <button onClick={() => setShowConfirmation(true)} id="loginspec">Log Out</button>
+            {showConfirmation && (
+                <div>
+                    <p>Are you sure you want to log out?</p>
+                    <button onClick={handleLogout}>Yes</button>
+                    <button onClick={() => setShowConfirmation(false)}>Cancel</button>
+                </div>
+            )}
         </div>
     );
 }
