2b1370074621357aff220f4856a2179762fb639a
derwonderwoman <tokarevakseniia@gmail.com>
2024-06-09 20:17:37 +0300

    logout

--- Layout/HomePage/src/components/HomePage/Logout.tsx
+++ Layout/HomePage/src/components/HomePage/Logout.tsx
@@ -1,11 +1,27 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../App";
 
 const LogOut = () => {
+    const { setLoggedInUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    setLoggedInUser({
+        id:0,
+        email:"",
+        first_name:"",
+        last_name:"",
+        phone_number:"",
+        town:"",
+        type:"client",
+        token: ""
+    });
+
+    localStorage.removeItem('loggedInUser');
+    navigate("/");
     return (
         <div>
-            <Link to="/specialist/login">
-                <button id="loginspec">Log Out</button>
-            </Link>
+            <button id="loginspec">Log Out</button>
         </div>
     );
 }
