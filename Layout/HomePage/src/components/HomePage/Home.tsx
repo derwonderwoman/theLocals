11aeec363234695d2da649e9efbaf930b30db5a5
derwonderwoman <tokarevakseniia@gmail.com>
2024-06-09 21:22:31 +0300

    logout

--- Layout/HomePage/src/components/HomePage/Home.tsx
+++ Layout/HomePage/src/components/HomePage/Home.tsx
@@ -3,35 +3,17 @@ import LoginClient from './LoginClient';
 import LoginSpec from './LoginSpec';
 import Title from './Title';
 import WelcomeInput from './WelcomeInput';
-import { useContext } from "react";
-import { AuthContext } from "../../App";
-import LogOut from "./Logout";
 
 
 const Home = () => {
-  const {loggedInUser} = useContext(AuthContext);
 
   return (
     <>
       <Title/>
       <CallForRegistration/>
       <WelcomeInput/>
-      {
-        !loggedInUser || loggedInUser.id === 0 && (
-          <>
-              <LoginSpec/>
-              <LoginClient/>
-          </>
-        )
-      }
-      {
-        loggedInUser && loggedInUser.id !== 0 && (
-          <>
-              <LogOut/>
-          </>
-        )
-      }
-
+      <LoginSpec/>
+      <LoginClient/>
     </>
   )
 }
